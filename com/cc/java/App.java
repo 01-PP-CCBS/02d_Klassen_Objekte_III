4596430a97d78a8d6220614103defdaa50e94eb3
jan <jaco@emailn.de>
2024-05-24 10:52:38 +0200

    01 / if-else / boolean is female

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Grizabella", "white", 29);
+        Cat cat = new Cat("Grizabella", "white", 29, true);
 
         output(cat.getStringAttributes("#name"));
         output(cat.getStringAttributes("#color"));
@@ -15,13 +15,12 @@ public class App {
 
         output("-------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "grey", 35);
+        Cat cat2 = new Cat("Alonzo", "grey", 35, false);
 
         output(cat2.getStringAttributes("#name"));
         output(cat2.getStringAttributes("#color"));
 
         output(cat2.getAge()); 
-
     }
 
     private static void output(String outputStr) {
