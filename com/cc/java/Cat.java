0912a6bab1d13c22a0d824d65d051c9d1219e5b9
jan <jaco@emailn.de>
2024-05-24 12:39:18 +0200

    03 / class Cat / Tomcat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,19 +1,19 @@
+// CAT
+
 package com.cc.java;
 
 public class Cat {
 
   private String name;
   private String furColor;
-  private int age;  
-  private boolean isFemale;
+  private int age; 
 
+  private int counter;
 
-  
-public Cat(String name, String furColor, int age, boolean isFemale) {
+public Cat(String name, String furColor, int age) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
 public String getStringAttributes(String op) {
@@ -28,15 +28,26 @@ public String getStringAttributes(String op) {
 }
 
 public String getAge() {
-  if (isFemale) {
-    return "This is an inappropriate question!";
-  } else {
-    return String.valueOf(age);    
+    return checkCompliance();
+}
+
+private String checkCompliance(){
+
+  counter++;
+
+  switch (counter) {
+    case 1:
+      return "This is an inappropriate question!"; // 1st time
+    case 2:
+      return "I've told you once!"; // 2nd time
+    case 3:
+      return "Talk to the hand!"; // 3rd time
+    default:
+      return "1#!?&&%"; //4th ++
   }
-
-
-  }
-
+  
+}
 
 }
   
+
